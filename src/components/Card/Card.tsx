c385c696702e09ea28dff9215eadd5b130ce268d
unknown <anour2168@gmail.com>
2023-03-29 01:53:10 +0200

    finish Cart that exists in Product Card and Navbar

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,6 +4,8 @@ import product3 from "../../assets/images/product3.png";
 import product4 from "../../assets/images/product4.png";
 import product5 from "../../assets/images/product5.png";
 import product6 from "../../assets/images/product6.png";
+import greenCart from "../../assets/images/greenCart.svg";
+
 const data = [
   {
     id: 1,
@@ -38,9 +40,18 @@ const Card = () => {
           return (
             <div
               key={item.id}
-              className="p-3 font-raleway flex flex-col my-5 hover:shadow-2xl translate-y-5 transition  duration-300"
+              className="card p-3 font-raleway flex flex-col my-5 hover:shadow-2xl translate-y-5 transition  duration-300 cursor-pointer"
             >
               <img src={item.img} alt="Product Img" />
+
+              <div className="cart relative cursor-pointer hidden transition duration-300">
+                <img
+                  src={greenCart}
+                  alt="Cart"
+                  className="absolute -bottom-10 right-0 md:w-16 md:h-16 md:-bottom-9"
+                />
+              </div>
+
               <div className="py-5">
                 <h2 className="font-normal text-lg">Apollo Running Short</h2>
                 <p className="font-semibold text-lg">$50.00</p>
